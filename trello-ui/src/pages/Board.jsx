771eb44ca2c01d41245465f4704b085829a89c1e
ChiNg22nd04 <dedonai99@gmail.com>
2025-07-11 20:58:35 +0700

    Fetch and render in board page

--- trello-ui/src/pages/Board.jsx
+++ trello-ui/src/pages/Board.jsx
@@ -1,11 +1,33 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import API_BASE_URL from "../../config/index";
+
 import { BoardCard, Sidebar, Header } from "../components";
 
+import { useUser } from "../hooks";
+
 const BoardPage = () => {
+    const user = useUser();
     const headerHeight = "60px";
+    const [boards, setBoards] = useState([]);
+
+    useEffect(() => {
+        if (!user?.id) return;
+
+        const fetchBoards = async () => {
+            const response = await axios.get(`${API_BASE_URL}/boards`, {
+                params: user.id,
+            });
+            setBoards(response.data);
+        };
+
+        fetchBoards();
+    }, [user]);
 
     return (
         <>
-            <Header username="Chi" style={{ height: headerHeight, zIndex: 1030 }} />
+            <Header username={user.username} style={{ height: headerHeight, zIndex: 1030 }} />
 
             <div
                 className="d-flex bg-dark text-white"
@@ -28,31 +50,9 @@ const BoardPage = () => {
                 >
                     <h6 className="text-secondary fw-bold mb-4">YOUR WORKSPACES</h6>
                     <div className="d-flex flex-wrap">
-                        <BoardCard title="My Trello board" />
-                        <BoardCard title="+ Create a new board" />
-                        <BoardCard title="My Trello board" />
-                        <BoardCard title="+ Create a new board" />
-                        <BoardCard title="My Trello board" />
-                        <BoardCard title="+ Create a new board" />
-                        <BoardCard title="My Trello board" />
-                        <BoardCard title="+ Create a new board" />
-                        <BoardCard title="My Trello board" />
-                        <BoardCard title="+ Create a new board" />
-                        <BoardCard title="My Trello board" />
-                        <BoardCard title="+ Create a new board" />
-                        <BoardCard title="My Trello board" />
-                        <BoardCard title="+ Create a new board" />
-                        <BoardCard title="My Trello board" />
-                        <BoardCard title="+ Create a new board" />
-                        <BoardCard title="My Trello board" />
-                        <BoardCard title="+ Create a new board" />
-                        <BoardCard title="My Trello board" />
-                        <BoardCard title="+ Create a new board" />
-                        <BoardCard title="My Trello board" />
-                        <BoardCard title="+ Create a new board" />
-                        <BoardCard title="My Trello board" />
-                        <BoardCard title="+ Create a new board" />
-                        <BoardCard title="My Trello board" />
+                        {boards.map((board) => (
+                            <BoardCard key={board.id} title={board.name} description={board.description} />
+                        ))}
                         <BoardCard title="+ Create a new board" />
                     </div>
                 </div>
