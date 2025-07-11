a0ab42fad56ec17131dea82d15d4229bdef42ccb
ChiNg22nd04 <dedonai99@gmail.com>
2025-07-11 11:14:52 +0700

    update endpoint card

--- trello-api/routes/card.routes.js
+++ trello-api/routes/card.routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 const { cardController } = require("../controllers");
 
 const authMiddleware = require("../middleware/auth.middleware.js");
 
 router.get("/", authMiddleware, cardController.getCards);
 router.post("/", authMiddleware, cardController.createCard);
+router.get("/:id", authMiddleware, cardController.getCardById);
+router.get("/user/:userId", authMiddleware, cardController.getCardByUser);
 
 module.exports = router;
