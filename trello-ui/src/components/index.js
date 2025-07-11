95eb30d22f65fdb92cd7f9a155305cac3a30d397
ChiNg22nd04 <dedonai99@gmail.com>
2025-07-11 19:56:46 +0700

    fix bug import component

--- trello-ui/src/components/index.js
+++ trello-ui/src/components/index.js
@@ -1,6 +1,5 @@
 import BoardCard from "./BoardCard";
 import Sidebar from "./Sidebar";
-import Topbar from "./Topbar";
 import Header from "./Header";
 
-export { BoardCard, Sidebar, Topbar, Header };
+export { BoardCard, Sidebar, Header };
